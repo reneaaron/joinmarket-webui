8228c4e18f5d1ca4b071e39d3cd33c0a4e012577
Daniel <10026790+dnlggr@users.noreply.github.com>
2022-04-12 17:03:39 +0200

    Ignore copy of backend repo

--- docker/regtest/dockerfile-deps/joinmarket/latest/joinmarket-clientserver/scripts/joinmarket-qt.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd $(dirname "$0")/.. && \
-source jmvenv/bin/activate && \
-cd scripts && \
-python3 joinmarket-qt.py
--- docker/regtest/dockerfile-deps/joinmarket/latest/joinmarket-clientserver/scripts/wallet-tool.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from jmbase import jmprint
-from jmclient import wallet_tool_main
-
-if __name__ == "__main__":
-    jmprint(wallet_tool_main("wallets"), "success")
