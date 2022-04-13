caa8fe48616883cc654f8d27354f540a6d663d8c
theborakompanioni <theborakompanioni+github@gmail.com>
2022-04-13 14:27:53 +0200

    add advanced Fidelity Bond screen - WIP

--- src/components/FidelityBond.jsx
+++ src/components/FidelityBond.jsx
@@ -33,9 +33,16 @@ const DepositTemplate = ({ title, amount, locktime, ...props }) => {
   )
 }
 
-const dateToLocktime = (date) => `${date.getUTCFullYear()}-${1 + date.getUTCMonth()}`
+const dateToLocktime = (date) =>
+  `${date.getUTCFullYear()}-${date.getUTCMonth() >= 9 ? '' : '0'}${1 + date.getUTCMonth()}`
 
-const LocktimeForm = ({ onChange }) => {
+// a maximum of years for a timelock to be accepted
+// this is useful in simple mode - when it should be prevented that users
+// lock up their coins for an awful amount of time by accident.
+// in "advanced" mode, this can be dropped or increased substantially
+const DEFAULT_MAX_TIMELOCK_YEARS = 5
+
+const LocktimeForm = ({ onChange, maxYears = DEFAULT_MAX_TIMELOCK_YEARS }) => {
   const { t } = useTranslation()
 
   const now = new Date()
@@ -60,39 +67,46 @@ const LocktimeForm = ({ onChange }) => {
   }
 
   return (
-    <>
-      <rb.Form.Group className="mb-4" controlId="locktimeYear">
-        <rb.Form.Label form="fidelity-bond-form">{t('fidelity_bond.form_create.label_locktime_year')}</rb.Form.Label>
-        <rb.Form.Control
-          name="amount"
-          type="number"
-          value={locktimeYear}
-          min={currentYear}
-          placeholder={t('fidelity_bond.form_create.placeholder_locktime_year')}
-          required
-          onChange={(e) => setLocktimeYear(parseInt(e.target.value, 10))}
-        />
-        <rb.Form.Control.Feedback type="invalid">
-          {t('fidelity_bond.form_create.feedback_invalid_locktime_year')}
-        </rb.Form.Control.Feedback>
-      </rb.Form.Group>
-      <rb.Form.Group className="mb-4" controlId="locktimeMonth">
-        <rb.Form.Label form="fidelity-bond-form">{t('fidelity_bond.form_create.label_locktime_month')}</rb.Form.Label>
-        <rb.Form.Control
-          name="amount"
-          type="number"
-          value={locktimeMonth}
-          min={minMonth()}
-          max={12}
-          placeholder={t('fidelity_bond.form_create.placeholder_locktime_month')}
-          required
-          onChange={(e) => setLocktimeMonth(parseInt(e.target.value, 10))}
-        />
-        <rb.Form.Control.Feedback type="invalid">
-          {t('fidelity_bond.form_create.feedback_invalid_locktime_month')}
-        </rb.Form.Control.Feedback>
-      </rb.Form.Group>
-    </>
+    <rb.Row>
+      <rb.Col xs={6}>
+        <rb.Form.Group className="mb-4" controlId="locktimeYear">
+          <rb.Form.Label form="fidelity-bond-form">{t('fidelity_bond.form_create.label_locktime_year')}</rb.Form.Label>
+          <rb.Form.Control
+            name="amount"
+            type="number"
+            value={locktimeYear}
+            min={currentYear}
+            max={currentYear + maxYears}
+            placeholder={t('fidelity_bond.form_create.placeholder_locktime_year')}
+            required
+            onChange={(e) => setLocktimeYear(parseInt(e.target.value, 10))}
+            isInvalid={locktimeYear < currentYear || locktimeYear > currentYear + maxYears}
+          />
+          <rb.Form.Control.Feedback type="invalid">
+            {t('fidelity_bond.form_create.feedback_invalid_locktime_year')}
+          </rb.Form.Control.Feedback>
+        </rb.Form.Group>
+      </rb.Col>
+      <rb.Col xs={6}>
+        <rb.Form.Group className="mb-4" controlId="locktimeMonth">
+          <rb.Form.Label form="fidelity-bond-form">{t('fidelity_bond.form_create.label_locktime_month')}</rb.Form.Label>
+          <rb.Form.Control
+            name="amount"
+            type="number"
+            value={locktimeMonth}
+            min={minMonth()}
+            max={12}
+            placeholder={t('fidelity_bond.form_create.placeholder_locktime_month')}
+            required
+            onChange={(e) => setLocktimeMonth(parseInt(e.target.value, 10))}
+            isInvalid={locktimeMonth < minMonth()}
+          />
+          <rb.Form.Control.Feedback type="invalid">
+            {t('fidelity_bond.form_create.feedback_invalid_locktime_month')}
+          </rb.Form.Control.Feedback>
+        </rb.Form.Group>
+      </rb.Col>
+    </rb.Row>
   )
 }
 
@@ -133,17 +147,39 @@ const DepositForm = ({ title, accounts, ...props }) => {
             Amount
             <Balance valueString={`${amount}`} convertToUnit={settings.unit} showBalance={settings.showBalance} />
           </rb.Col>
-          <rb.Col xs={'auto'}>
-            Duration
-            {locktime}
-          </rb.Col>
+          <rb.Col xs={'auto'}>Expires at: {locktime}</rb.Col>
         </rb.Row>
       </rb.Card.Body>
     </rb.Card>
   )
 }
 
-export default function FidelityBond() {
+const DepositFormAdvanced = ({ title, ...props }) => {
+  const { t } = useTranslation()
+  const now = new Date()
+  const year = new Date().getUTCFullYear()
+  const month = new Date().getUTCMonth()
+  const initialLocktimeDate = new Date(Date.UTC(year + 1, month, 1, 0, 0, 0))
+
+  const settings = useSettings()
+  const [locktime, setLocktime] = useState(dateToLocktime(initialLocktimeDate))
+
+  return (
+    <rb.Card {...props}>
+      <rb.Card.Body>
+        <rb.Card.Title>{title}</rb.Card.Title>
+        <rb.Form noValidate>
+          <LocktimeForm onChange={setLocktime} />
+        </rb.Form>
+        <rb.Row>
+          <rb.Col>Expires at: {locktime}</rb.Col>
+        </rb.Row>
+      </rb.Card.Body>
+    </rb.Card>
+  )
+}
+
+const FidelityBondSimple = () => {
   const { t } = useTranslation()
   const settings = useSettings()
   const currentWallet = useCurrentWallet()
@@ -183,6 +219,99 @@ export default function FidelityBond() {
   }, [currentWallet, reloadCurrentWalletInfo, t])
 
   return (
+    <div>
+      {isLoading && (
+        <div className="d-flex justify-content-center align-items-center">
+          <rb.Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" className="me-2" />
+          {t('loading')}
+        </div>
+      )}
+      {alert && <rb.Alert variant={alert.variant}>{alert.message}</rb.Alert>}
+      {infoAlert && <rb.Alert variant={infoAlert.variant}>{infoAlert.message}</rb.Alert>}
+
+      <rb.Fade in={fidelityBonds && fidelityBonds.length === 0} mountOnEnter={true} unmountOnExit={true}>
+        <>
+          <DepositTemplate title="Long Term" amount="123" locktime="2042-01" />
+          <DepositTemplate title="Short Term" amount="123" locktime="2042-01" />
+          <DepositForm title="form" />
+        </>
+      </rb.Fade>
+    </div>
+  )
+}
+
+const FidelityBondAdvanced = () => {
+  const { t } = useTranslation()
+  const settings = useSettings()
+  const currentWallet = useCurrentWallet()
+  const reloadCurrentWalletInfo = useReloadCurrentWalletInfo()
+  const [fidelityBonds, setFidelityBonds] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [alert, setAlert] = useState(null)
+  const [infoAlert, setInfoAlert] = useState(null)
+
+  useEffect(() => {
+    if (!currentWallet) {
+      setAlert({ variant: 'danger', message: t('current_wallet.error_loading_failed') })
+      setIsLoading(false)
+      return
+    }
+
+    const abortCtrl = new AbortController()
+
+    setAlert(null)
+    setIsLoading(true)
+    setFidelityBonds(null)
+
+    reloadCurrentWalletInfo({ signal: abortCtrl.signal })
+      .then((info) => {
+        if (info) {
+          const timelockedUtxos = info.data.utxos.utxos.filter((utxo) => utxo.locktime)
+          setFidelityBonds(timelockedUtxos)
+        }
+      })
+      .catch((err) => {
+        const message = err.message || t('current_wallet.error_loading_failed')
+        !abortCtrl.signal.aborted && setAlert({ variant: 'danger', message })
+      })
+      .finally(() => !abortCtrl.signal.aborted && setIsLoading(false))
+
+    return () => abortCtrl.abort()
+  }, [currentWallet, reloadCurrentWalletInfo, t])
+
+  return (
+    <div>
+      <p>
+        You are in advanced mode. It is assumed that you know what you are doing. If you are not sure, read{' '}
+        <a
+          href="https://github.com/JoinMarket-Org/joinmarket-clientserver/blob/master/docs/fidelity-bonds.md"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-secondary"
+        >
+          the documentation
+        </a>
+        .
+      </p>
+      {isLoading && (
+        <div className="d-flex justify-content-center align-items-center">
+          <rb.Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" className="me-2" />
+          {t('loading')}
+        </div>
+      )}
+      {alert && <rb.Alert variant={alert.variant}>{alert.message}</rb.Alert>}
+      {infoAlert && <rb.Alert variant={infoAlert.variant}>{infoAlert.message}</rb.Alert>}
+
+      <DepositFormAdvanced title="form" />
+    </div>
+  )
+}
+
+export default function FidelityBond() {
+  const { t } = useTranslation()
+  const settings = useSettings()
+
+  return (
     <div className="fidelity-bond">
       <rb.Row>
         <rb.Col>
@@ -198,22 +327,7 @@ export default function FidelityBond() {
             </a>
           </p>
 
-          {isLoading && (
-            <div className="d-flex justify-content-center align-items-center">
-              <rb.Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" className="me-2" />
-              {t('loading')}
-            </div>
-          )}
-          {alert && <rb.Alert variant={alert.variant}>{alert.message}</rb.Alert>}
-          {infoAlert && <rb.Alert variant={infoAlert.variant}>{infoAlert.message}</rb.Alert>}
-
-          <rb.Fade in={fidelityBonds && fidelityBonds.length === 0} mountOnEnter={true} unmountOnExit={true}>
-            <>
-              <DepositTemplate title="Long Term" amount="123" locktime="2042-01" />
-              <DepositTemplate title="Short Term" amount="123" locktime="2042-01" />
-              <DepositForm title="form" />
-            </>
-          </rb.Fade>
+          {settings.useAdvancedWalletMode ? <FidelityBondAdvanced /> : <FidelityBondSimple />}
         </rb.Col>
       </rb.Row>
     </div>
